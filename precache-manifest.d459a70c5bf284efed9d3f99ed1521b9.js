e08c84e34eb6600e94a6fd61c7f9366c15157ae3
nikeshthapa255 <nikeshthapa255@gmail.com>
2019-07-20 03:05:34 +0530

    Updates

---
+++ precache-manifest.d459a70c5bf284efed9d3f99ed1521b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d62ce3d371a51794bf6d5cb7185d11",
+    "url": "/reactpractice/index.html"
+  },
+  {
+    "revision": "66085b9dbfd274b14e2a",
+    "url": "/reactpractice/static/css/2.5ba5ddee.chunk.css"
+  },
+  {
+    "revision": "b7971832b2457112920a",
+    "url": "/reactpractice/static/css/main.528f7ac2.chunk.css"
+  },
+  {
+    "revision": "66085b9dbfd274b14e2a",
+    "url": "/reactpractice/static/js/2.b22b24da.chunk.js"
+  },
+  {
+    "revision": "b7971832b2457112920a",
+    "url": "/reactpractice/static/js/main.d9452fd2.chunk.js"
+  },
+  {
+    "revision": "0bc16deefa99f8785e54",
+    "url": "/reactpractice/static/js/runtime~main.94f820ca.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactpractice/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactpractice/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactpractice/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactpractice/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactpractice/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
